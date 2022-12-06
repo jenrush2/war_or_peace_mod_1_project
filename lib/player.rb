adf7154c9d589239c56ec10d544543866afd9025
Jennifer Rush <jennifer.rush2@gmail.com>
2022-12-05 19:06:28 -0700

    Finish player class

--- lib/player.rb
+++ lib/player.rb
@@ -9,35 +9,13 @@ class Player
         @deck = deck
     end
 
-    def name
-        @name
+    def has_lost?
+        if deck.cards.length <= 0
+            return true
+        else
+            return false
+        end
     end
-
-    def deck
-        @deck
-    end
-
-    #def has_lost?
-       # if deck.length <= 0
-            #return true
-        #else
-           # return false
-        #end
-   # end
 
 end
 
-
-card1 = Card.new(:diamond, 'Queen', 12)
-card2 = Card.new(:spade, '3', 3)
-card3 = Card.new(:heart, 'Ace', 14)
-
-cards = [card1, card2, card3]
-
-deck = Deck.new(cards)
-
-player = Player.new('Clarisa', deck)
-
-p player.name
-p player.deck
-#p player.has_lost?
