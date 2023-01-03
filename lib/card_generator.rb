81696cee4a048e35b7ff01eb3ac011e1de2868d6
Jennifer Rush <jennifer.rush2@gmail.com>
2023-01-03 16:56:34 -0700

    Create first iteration of create_cards: one card

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -13,11 +13,21 @@ class CardGenerator
     end
 
     def create_cards
+        card_deck = []
         File.foreach(filename) {|line|
-        new_card = Card.new(line)
-        card_deck << Deck.new(new_card)
-        card_deck.cards
+            full_list_split = line.split(",").map(&:strip)
+            values_for_one_card = []
+            values_for_one_card << full_list_split.slice(0..2)
+            values_for_one_card = values_for_one_card.flatten
+            this_suit = values_for_one_card[0].to_sym
+            this_value = values_for_one_card[1]
+            this_rank = values_for_one_card[2].to_i
+            
+            
+            new_card = Card.new(this_suit, this_value, this_rank)
+            card_deck = Deck.new(new_card)
         }
+        card_deck.cards
     end
                    
 end
